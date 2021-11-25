137d22a722eb175e4c884f7e6f3148ef2939b237
Raul Ulici <raululici@yahoo.com>
2021-11-25 17:44:41 +0200

    problema partiala

---
+++ Vectori 4/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Vectori_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] fibo = Fibonacci(5);
+            Console.WriteLine(fibo);
+            for(int i = 1; i<= fibo.Length; i++)
+            {
+                if (fibo[i] != 0)
+                    Console.WriteLine(fibo[i]);
+            }
+        }
+
+        static int[] Fibonacci(int n)
+        {
+            int[] v = new int[100];
+            v[0] = 0;
+            if (n == 0)
+                return v;
+            v[1] = 1;
+            if (n == 1)
+                return v;
+            int i = 1;
+            while(v[i]<n)
+            {
+                i++;
+                v[i] = v[i - 1] + v[i - 2];
+            }
+            return v;
+        }
+    }
+}
